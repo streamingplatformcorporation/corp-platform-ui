16e4a8315a544ba4ee8cbcba9a0a3f3af6e6ff45
Lydia <lattwood@rams-pro.com>
2020-01-21 19:54:22 -0700

    Streaming layout, header, home, routed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './_layout/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ContactComponent,
+        AboutComponent,
+        HeaderComponent
+    ],
+
+    imports: [
+
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+
+                path: 'home',
+                component: HomeComponent,
+            },
+
+            {
+                path: 'contact',
+                component: ContactComponent,
+
+            },
+
+            {
+                path: 'about',
+                component: AboutComponent,
+
+            },
+
+        ]),
+    ],
+
+    providers: [],
+    bootstrap: [ AppComponent ]
+
 })
-export class AppModule { }
+export class AppModule {
+}
